9e46f7d22199517effe2fbe2cd8895d4ff5dac4b
vnaveensukumarreddy <vnaveensukumarreddy@gmail.com>
2020-09-21 23:36:27 +0530

    Added Successfully

--- JAVA LAB ASSIGNMENTS/DAY 06/src/com/main/NumberApp.java
+++ JAVA LAB ASSIGNMENTS/DAY 06/src/com/main/NumberApp.java
@@ -1,12 +1,18 @@
 package com.main;
+
 import com.threads.Number;
+
 public class NumberApp {
 
 	public static void main(String[] args) {
-		
-		Thread thread1=new Thread(new Number(2));
-		Thread thread2=new Thread(new Number(5));
-		Thread thread3=new Thread(new Number(8));
+
+		Thread thread1 = new Thread(new Number(2));
+		Thread thread2 = new Thread(new Number(5));
+		Thread thread3 = new Thread(new Number(8));
+		thread1.setName("2 multiple");
+		thread2.setName("5 multiple");
+		thread3.setName("8 Multiple");
+
 		thread1.start();
 		try {
 			thread1.join();
@@ -16,8 +22,6 @@ public class NumberApp {
 		}
 		thread2.start();
 		thread3.start();
-		
 
 	}
-
 }
--- JAVA LAB ASSIGNMENTS/DAY 06/src/com/threads/Number.java
+++ JAVA LAB ASSIGNMENTS/DAY 06/src/com/threads/Number.java
@@ -10,11 +10,19 @@ public class Number implements Runnable {
 @Override
 public void run() {
 	
+	System.out.println("Starting  "+ Thread.currentThread().getName());
 	for(int i=1;i<=10;i++)
 	{
+		
 		System.out.println(multiple +" * "+i+" :" +multiple*i);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+	System.out.println("Stopping  "+ Thread.currentThread().getName());
 }
 
 }
